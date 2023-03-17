2d7642c605ec446f3f2458ccb37a5f96acfda0f5
chino702 <chinox3702@gmail.com>
2023-03-17 15:03:46 -0700

    control lab phase 1

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result = "This one is on me!";
+  if (someValue <= 400) return result;
+  else if(someValue <= 2000) return("That will be twenty bucks.");
+  else if(someValue <= 2500) return("I will gladly take your thirty bucks.");
+  else if(someValue >= 2500) return("No can do.");
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ if (city == "NYC") return "Ok, sounds good.";
+ else if (city == "Pittsburgh") return "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  
+  let response;
+
+  switch (tip){
+    case "generous":
+      response = "Thank you so much.";
+      break;
+      case "not as generous":
+        response = "Thank you.";
+        break;
+        case "thanks for everything":
+          response = "Bye.";
+          break;
+  };
+  return response;
+  };
